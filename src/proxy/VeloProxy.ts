53116d45d2026a4cb8bc675b4a627b57c4c750c2
Bavfalcon9 <olybear9@gmail.com>
2020-07-07 18:45:25 -0500

    d

---
+++ src/proxy/VeloProxy.ts
@@ -0,0 +1,13 @@
+// this is bound to change.
+class VeloProxy {
+     /**
+      * start
+      */
+     public start() {
+          // we can't do this yet because deno doesn't have any libs for udp4 sockets
+     }
+
+     public send(stream: BinaryStream): void {
+          
+     }
+}
---
+++ start.ps1
@@ -0,0 +1,7 @@
+param ($file='./tests/ProxyTest.ts')
+try {
+     Write-Host "Executing script with deno" -ForegroundColor Yellow
+     deno.exe run -A $file
+} catch {
+     Write-Host "Error: Deno not found (or other)" -ForegroundColor Red
+     timeout.exe /t 5 /nobreak
