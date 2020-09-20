25e22ee4c1061de13098354e16ea312b209b94c0
Cqdet <60723938+Cqdet@users.noreply.github.com>
2020-09-20 09:57:59 -0400

    Update PlayerLoginPacket.ts
    Much of this is still WIP and based of PMMP impl

--- lib/packets/PlayerLoginPacket.ts
+++ lib/packets/PlayerLoginPacket.ts
@@ -1,12 +1,29 @@
-import Packet from "../../impl/RakNet/lib/protocol/Packet.ts";
+import BinaryStream from "https://raw.githubusercontent.com/RaptorsMC/BinaryUtils/master/lib/BinaryStream.ts";
+import DataPacket from "../../impl/RakNet/lib/protocol/DataPacket.ts";
+import Identifiers from "../../impl/RakNet/lib/protocol/Identifiers.ts";
+import { Address } from "../../impl/RakNet/mod.ts";
 
-class PlayerLoginPacket extends Packet {
-    constructor(id: number) {
-        super(id)
+class PlayerLoginPacket extends DataPacket {
+    public id: number = Identifiers.PlayerStartGamePacket;
+
+    public username!: string;
+    public protocol!: number;
+    public clientID!: string;
+    public serverAddr!: Address;
+
+    public data!: string[][] | object;
+
+
+    public read() {
+        this.protocol = this.readInt();
+        this.serverAddr = this.readAddress();
+        
+        this.decodeConnection();
     }
 
-    read() {
-        // Still confused on how to implement this
+    public decodeConnection() {
+        let buf = new BinaryStream();
+        this.data = buf.read(buf.readLInt()).toJSON();
     }
 }
 export default PlayerLoginPacket;
