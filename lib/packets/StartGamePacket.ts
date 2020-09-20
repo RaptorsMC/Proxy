04367c8f85462b23431ee0623de6c5ab7aba0e2e
Cqdet <60723938+Cqdet@users.noreply.github.com>
2020-09-20 09:58:27 -0400

    Update StartGamePacket.ts
    Much of this is WIP and based of PMMP imp

--- lib/packets/StartGamePacket.ts
+++ lib/packets/StartGamePacket.ts
@@ -1,12 +1,25 @@
+import DataPacket from "../../impl/RakNet/lib/protocol/DataPacket.ts";
+import Identifiers from "../../impl/RakNet/lib/protocol/Identifiers.ts";
 import Packet from "../../impl/RakNet/lib/protocol/Packet.ts";
 
-class StartGamePacket extends Packet {
-    constructor(id: number) {
-        super(id)
-    }
+class StartGamePacket extends DataPacket {
+    public id: number = Identifiers.PlayerStartGamePacket;
+    public vanillaVersion!: any; 
+    /**
+     * We will add more properties onto this packet soon
+     * Much of this is based off the PMMP impl
+     */
 
-    read() {
-        // Still confused on how to implement this
+    public read() {
+        super.read();
+        this.vanillaVersion = this.readString(); 
+        // I just want to read the string but this returns void :(
+        // Ok so now
+        
+    }
+    
+    public write() {
+        super.write();
     }
 }
 export default StartGamePacket;
